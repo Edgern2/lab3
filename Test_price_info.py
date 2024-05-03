162426bd4b851555fa1b88d26c5bbc90cfc5cd91
Xin <chengxinjun759@gmail.com>
2024-05-04 01:03:19 +0800

    added price info

---
+++ Test_price_info.py
@@ -0,0 +1,9 @@
+import price_info
+
+def total_cost_shopping():
+
+    assert 46.75 == price_info.total_cost_shopping
+    
+def cost_of_fruit():
+    
+    assert 12.0 == price_info.cost_of_fruits("apple,10")
